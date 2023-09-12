f6628fd243d05e4c9e2a08df47a66f38e8357dd4
GabrielCasillas1225 <126525999+GabrielCasillas1225@users.noreply.github.com>
2023-09-11 21:10:29 -0700

    Update ForgotPass.js

--- TodoAppReact/ForgotPass.js
+++ TodoAppReact/ForgotPass.js
@@ -1,1 +1,62 @@
+import logo1 from './logo1.png';
+import './Forgot.css'
 
+function ForgotPass() {
+    return (
+      <>
+
+  <div className="App">
+      <div className="contenedor">
+
+      <img src={logo1} className="applogo" alt="logo" />
+      
+      </div>
+      <div className="forgotpasstitulo">
+
+      <h1>Forgot Password</h1>
+
+      </div>
+
+      
+            <br></br>
+            <br></br>
+            <div className="contenedornombre">
+
+            <label className='emaildis' htmlFor="email">Email:</label>
+      <input
+            type="text"
+            id="email"
+            name="email"
+            />
+              
+            </div>
+
+            <br></br>
+            <br></br>
+
+      <div className="botones">
+          
+          <button className="enviardis" type="submit">Enviar</button>
+          
+
+        </div>
+
+        <p className='login'>Login</p>
+
+        
+
+    </div>
+
+
+
+
+
+
+
+
+
+      </>
+    );
+  }
+  
+  export default ForgotPass;
