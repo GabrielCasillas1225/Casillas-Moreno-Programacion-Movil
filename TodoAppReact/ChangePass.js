6c4a2661443595b7e5d7ebcef7e53ac5a9d6f8c6
GabrielCasillas1225 <126525999+GabrielCasillas1225@users.noreply.github.com>
2023-09-11 20:52:09 -0700

    Update ChangePass.js

--- TodoAppReact/ChangePass.js
+++ TodoAppReact/ChangePass.js
@@ -1,1 +1,93 @@
+import logo1 from './logo1.png';
+import './ChangePass.css';
 
+function ChangePass() {
+    return (
+      <>
+
+<div className='App'>
+
+<div className="contenedor">
+
+<img src={logo1} className="applogo" alt="logo" />
+
+</div>
+<div className="changepasstitulo">
+
+<h1>Reset Password</h1>
+
+</div>
+
+
+      <br></br>
+      <br></br>
+
+      <div className="contenedorcontrasenia">
+
+
+
+<label className='contraseniadis' htmlFor="contrasenia">Contraseña:</label>
+<input
+      type="text"
+      id="contrasenia"
+      name="contrasenia"
+      />
+
+    </div>
+
+    <br></br>
+
+
+
+      <div className="nuevacontrasenia">
+
+      <label className='nuevacontraseniadis' htmlFor="nuevacontrasenia">Nueva Contraseña:</label>
+<input
+      type="text"
+      id="nuevacontrasenia"
+      name="nuevacontrasenia"
+      />
+        
+      </div>
+
+      <br></br>
+
+
+      <div className='confirmarcontrasenia'>
+
+      <label className='confirmarcontraseniadis' htmlFor="confirmarcontrasenia">Confirmar contraseña:</label>
+<input
+      type="text"
+      id="confirmarcontrasenia"
+      name="confirmarcontrasenia"
+      />
+
+      </div>
+
+      <br></br>
+      
+
+
+
+   
+      <br></br>
+      <br></br>
+
+<div className="botones">
+    
+    <button className="botonresetdis" type="submit">Reset</button>
+    
+
+  </div>
+
+ 
+
+
+  </div>
+
+
+      </>
+    );
+  }
+  
+  export default ChangePass;
